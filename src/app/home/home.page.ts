819a5b086a9934eeb8af3866fe276aa7d7bd5b51
KamadoSama <tahoarthur52@gmail.com>
2023-08-19 01:44:13 +0000

    #refactor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,46 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { TaskService } from '../shared/services/tasks.service';
-import { Observable } from 'rxjs';
-import { ITask } from '../shared/models/tasks';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage {
   title:string = 'Kamado BG'
   isOpen:boolean = true;
-  tasks$!: Observable<ITask[]>
+  tasks: any[] = []
   newTask!:string
-  constructor(private taskService: TaskService) {}
-
-  ngOnInit(): void {
-      this.taskService.getTasks().subscribe({
-        next:tasks=>{
-          console.log(tasks)
-          this.tasks = tasks
-        }
-      })
-  }
-  
-  // this.taskservice.getTasks().subscribe({
-  //   next: task=>{
-  //     this.tasks = task
-  //   }
-  // })
-
+  constructor() {}
   addNewTask(){
-
     let task = {
       isChecked:false,
       content:this.newTask
     }
-    console.log(task)
     this.newTask = ''
-    this.taskService.createTasks(task).subscribe({
-      next:task=>console.log('ok')
-    })
+    this.tasks.push(task)
   }
   onCheck(event:any,i:number){
     this.tasks[i].isChecked = event.detail.checked
@@ -48,7 +25,4 @@ export class HomePage implements OnInit{
   deleteTask(i:number){
     this.tasks.splice(i,1)
   }
-
-
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TaskService } from './services/tasks.service';
-import { HttpClientModule } from '@angular/common/http';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { TaskService } from './services/tasks.service';
+// import { HttpClientModule } from '@angular/common/http';
 
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,HttpClientModule
-  ],
-  providers:[TaskService]
-})
-export class SharedModule { }
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule,HttpClientModule
+//   ],
+//   providers:[TaskService]
+// })
+// export class SharedModule { }
